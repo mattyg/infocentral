1098eedc790f05e26ceea62d92ca69706f5eda4d
Matt Gabrenya <matt@gabrenya.com>
2010-11-25 00:56:13 -0500

    adding feedupdating abilities

--- src/dbconnection.py
+++ src/dbconnection.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python
+#dbconnection
 
-#dbconnect
+import sqlite3
+
+class dbconnection:
+	cursor = None
+	connection = None
+	
+	def __init__(this):
+		this.connection = sqlite3.connect('../storage.db')
+		this.cursor = this.connection.cursor()
+		
+	def getfeedurls(this): #get all feed urls, return in array
+		query = "SELECT * FROM feeds"
+		this.cursor.execute(query)
+		return this.cursor.fetchall()
+		
+	def additem(this,feedid,roleid,name,content,timestamp):
+		query = "INSERT INTO items (feedid,roleid,name,content,timestamp) VALUES ('%s','%s','%s','%s','%s')" %(feedid,roleid,name,content,timestamp)
+		#this.cursor.execute(query)
--- src/getdata.py
+++ src/getdata.py
@@ -1,3 +1,40 @@
 #!/usr/bin/python
-
 #get data input
+
+import sqlite3,feedparser,dbconnection
+
+class getdata:
+	dbconnection = None
+	
+	def __init__(this):
+		this.dbconnection = dbconnection.dbconnection()
+		this.update()
+		
+	def update(this): #poll all data feeds, add new data to db
+		#1 - get data feed urls from db
+		feedurls = this.dbconnection.getfeedurls()
+		print feedurls
+		#2 - check data feeds for new items
+		feeds = []
+		for feedurl in feedurls:
+			feeds.append(feedparser.parse(feedurl[1]))
+			print feeds[-1]
+			if feeds[-1]['entries']: #if there are new entries
+				#print feeds[-1]['entries']
+				for entry in feeds[-1]['entries']:
+					#print entry['summary_detail']['value'],'-',entry['title'],'-',entry[]
+					print entry,'\n'
+					#do analysis to get roleid
+					#add new data to db
+		
+		
+		#login to gcal feed with:
+		#https://www.google.com/accounts/ClientLogin
+		#
+		#The POST body should contain a set of query parameters, as described in the following table. They should look like parameters passed by an HTML form, using the application/x-www-form-urlencoded content type.
+		#Parameter 	Description
+		#Email 	The user's email address.
+		#Passwd 	The user's password.
+		#source 	Identifies your client application. Should take the form companyName-applicationName-versionID; below, we'll use the name exampleCo-exampleApp-1.
+		#service 	The string cl, which is the service name for Google Calendar.			
+getdata()
